1e7c74e37c659784c6749830ee0a4c23898d1cd0
Sami Rherbi <sami0771@edu.zealand.dk>
2022-09-05 13:04:36 +0200

    .

--- MyFirstGitHubClassRoomConsoleApp/Demo2.cs
+++ MyFirstGitHubClassRoomConsoleApp/Demo2.cs
@@ -8,6 +8,15 @@ namespace MyFirstGitHubClassRoomConsoleApp
 {
     class Demo2
     {
+        public int MyProperty { get; set; }
+
+        private int myVar;
+
+        public int MyProperty2
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
 
     }
 }
